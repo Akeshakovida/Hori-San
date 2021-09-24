c71604786438049096676e29f6d11377965bb518
Th30utlaw <abhiragh0@gmail.com>
2021-09-24 13:17:06 +0530

    Fix This Shit

---
+++ EzilaXBotV/modules/about.py
@@ -0,0 +1,12 @@
+__help__ = """
+** Hori-San - A Powerful Telegram Group Manager 🎶 **
+
+Powerful Abilities
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+Developer [Th30utlaw](https://t.me/Th30utlaw)
+"""
+__mod_name__ = "About"
--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-worker: python3 -m Natsuki
-web: python3 -m Natsuki
+worker: python3 -m EzilaXBotV
+web: python3 -m EzilaXBotV
 ps:scale worker=1
